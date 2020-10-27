b8becdd5f92d5115295abcd7eece8aeaa45c71c6
lfeighan <73256084+lfeighan@users.noreply.github.com>
2020-10-27 16:40:05 -0400

    Redirect to MAB Residencies on ORTVI

--- blk-blur-ui/src/BlkBlur/LandingPage.js
+++ blk-blur-ui/src/BlkBlur/LandingPage.js
@@ -94,7 +94,7 @@ const LandingPage = () => {
         </Grid>
         <Grid item>
           <Link
-            href="https://www.ortvi.art/programs/mab_residencies_t1"
+            href="https://www.ortvi.art/programs/tmab-resid-series-premium"
             underline="none"
             target="_blank"
           >
